329b3cb9f382c5a7d71ba8b4da2a77aee6bbbeaa
fsgdoterr <fsgoterr@gmail.com>
2021-07-27 18:23:18 +0300

    убрал картинку

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,9 +7,6 @@
                             <ul class="list big-list">
                                 @foreach($posts as $post)
                                 <li>
-                                    <a class="articles-image">
-                                        <img src="{{ asset($post->image_path) }}" alt="">
-                                    </a>
                                     <a href="{{ route('post.create', $post->slug) }}" class="articles-title"><h2>{{ $post->name }}</h2></a>
                                     <span class="post-date">{{ $post->created_at }}</span>
                                     <span class="post-description">{{ $post->description }}<a href="{{ route('post.create', $post->slug) }}"> [...]</a></span>
